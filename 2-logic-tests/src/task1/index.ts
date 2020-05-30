88c74603595b34dfd804ce5a0d0bacc2366e1250
Hubert Marzecki <hubert.marzecki@gmail.com>
2020-05-30 13:48:30 +0200

    adding new function

--- 2-logic-tests/src/task1/index.ts
+++ 2-logic-tests/src/task1/index.ts
@@ -1,149 +1,61 @@
-import {Transaction} from "../Model";
+import { Transaction } from "../Model";
 
 interface TransactionBasic {
-  amount: number,
-  category: string,
-  time: number,
+  amount: number;
+  category: string;
+  time: number;
 }
-var transacts = [
-  {
-    id: 3,
-    sourceAccount: 'A',
-    targetAccount: 'B',
-    amount: 100,
-    category: 'eating_out',
-    time: '2018-03-02T10:34:30.000Z'
-  },
-  {
-    id: 1,
-    sourceAccount: 'A',
-    targetAccount: 'B',
-    amount: 100,
-    category: 'eating_out',
-    time: '2018-03-02T10:33:00.000Z'
-  },
-  {
-    id: 6,
-    sourceAccount: 'A',
-    targetAccount: 'C',
-    amount: 250,
-    category: 'other',
-    time: '2018-03-02T10:33:05.000Z'
-  },
-  {
-    id: 4,
-    sourceAccount: 'A',
-    targetAccount: 'B',
-    amount: 100,
-    category: 'eating_out',
-    time: '2018-03-02T10:36:00.000Z'
-  },
-  {
-    id: 2,
-    sourceAccount: 'A',
-    targetAccount: 'B',
-    amount: 100,
-    category: 'eating_out',
-    time: '2018-03-02T10:33:50.000Z'
-  },
-  {
-    id: 5,
-    sourceAccount: 'A',
-    targetAccount: 'C',
-    amount: 250,
-    category: 'other',
-    time: '2018-03-02T10:33:00.000Z'
-  }
-];
-const transactsExpects = [
-  [
-    {
-      id: 1,
-      sourceAccount: "A",
-      targetAccount: "B",
-      amount: 100,
-      category: "eating_out",
-      time: "2018-03-02T10:33:00.000Z"
-    },
-    {
-      id: 2,
-      sourceAccount: "A",
-      targetAccount: "B",
-      amount: 100,
-      category: "eating_out",
-      time: "2018-03-02T10:33:50.000Z"
-    },
-    {
-      id: 3,
-      sourceAccount: "A",
-      targetAccount: "B",
-      amount: 100,
-      category: "eating_out",
-      time: "2018-03-02T10:34:30.000Z"
-    }
-  ],
-  [
-    {
-      id: 5,
-      sourceAccount: "A",
-      targetAccount: "C",
-      amount: 250,
-      category: "other",
-      time: "2018-03-02T10:33:00.000Z"
-    },
-    {
-      id: 6,
-      sourceAccount: "A",
-      targetAccount: "C",
-      amount: 250,
-      category: "other",
-      time: "2018-03-02T10:33:05.000Z"
-    }
-  ]
-];
 
 
-
-
-// https://gist.github.com/camisetags/10b5656411c683646193acdbb606535e
 export const getBalanceByCategoryInPeriod = (
-    transactionsList: Transaction[],
-    category: string,
-    startTime: string,
-    endTime: string
-) :number=> {
-
-//  czy potrzebne?
-function setEndDate(date) {
-  date = date ? new Date(date) : new Date();
-  // Zwraca godziny dla określonej daty stosownie do czasu uniwersalnego.
-  if (date.getUTCHours() <= 0){
-    date.setDate(date.getDate() + 1);
-    date.setHours(0);
-    date.setMinutes(0);
+  transactionsList: Transaction[],
+  category: string,
+  startTime: string,
+  endTime: string
+): number => {
+  //  czy potrzebne?
+  function setEndDate(date) {
+    date = date ? new Date(date) : new Date();
+    // Zwraca godziny dla określonej daty stosownie do czasu uniwersalnego.
+    if (date.getUTCHours() <= 0) {
+      date.setDate(date.getDate() + 1);
+      date.setHours(0);
+      date.setMinutes(0);
+    }
+    // convert date to object
+    return Date.parse(date);
   }
-  // convert date to object
-  return Date.parse(date);
 
-}
+  let startDate: number = Date.parse(startTime);
+  let endDate: number = Date.parse(endTime);
 
-  let startDate : number= Date.parse(startTime);
-  let endDate :number = Date.parse(endTime);
+  const transactionBasic: TransactionBasic[] = transactionsList.map(
+    (transfer: Transaction) => {
+      const {
+        id,
+        sourceAccount,
+        targetAccount,
+        ...transactionBasic
+      } = transfer;
+      return {
+        ...transactionBasic,
+        time: Date.parse(transactionBasic.time),
+      };
+    }
+  );
 
-  const transactionBasic :TransactionBasic[] = transactionsList.map((transfer:Transaction) => {
-    const { id, sourceAccount, targetAccount, ...transactionBasic } = transfer;
-    return {
-      ...transactionBasic,
-      time: Date.parse(transactionBasic.time)
-    };
-    })
+  const transactionInTime: TransactionBasic[] = transactionBasic.filter(
+    (transfer: TransactionBasic) =>
+      transfer.category === category &&
+      transfer.time >= startDate &&
+      transfer.time <= endDate
+  );
 
-  const transactionInTime:TransactionBasic[] = transactionBasic.filter((transfer:TransactionBasic) =>
-      (transfer.category === category &&
-          (transfer.time >= startDate && transfer.time <= endDate))
-  )
+  const balance: number = transactionInTime.reduce(
+    (cumulatedBalance: number, transfer: TransactionBasic) =>
+      cumulatedBalance + transfer.amount,
+    0
+  );
 
-  const balance : number= transactionInTime.reduce((cumulatedBalance:number, transfer:TransactionBasic) => cumulatedBalance + transfer.amount, 0);
-  
   return balance;
-      }
+};
--- 2-logic-tests/src/task2/index.ts
+++ 2-logic-tests/src/task2/index.ts
@@ -30,15 +30,13 @@ export const findDuplicateTransactions = (
   // patrzymy czy pasuje do ostatniego z zakresów transakcji
   // jezeli pasuje, to dodajemy do zakresu
   // jezeli nie, tworzymy nowy zakres
+  const compareTime = (a: Transaction, b: Transaction) => a.time.localeCompare(b.time);
 
-  const buckets: Array<Array<Transaction>> = [];
-  const sortedTransactions = transactions.sort(
-    (a: Transaction, b: Transaction) => a.time.localeCompare(b.time)
-  );
   function push<T>(arr: Array<T>, elem: T): T {
     arr.push(elem);
     return elem;
   }
+
   function getOrAdd<T>(
     arr: Array<T>,
     getter: (arr: Array<T>) => T | undefined,
@@ -54,22 +52,28 @@ export const findDuplicateTransactions = (
   function last<T>(arr: Array<T>): T | undefined {
     return arr[arr.length - 1];
   }
-  const putTransactionInLastCompatibleBucketOrNew = (
+  
+  function isLongerThan<T>(length: number): (arr: Array<T>)=> boolean {
+    return it => it.length > length
+  }
+
+  const computeTransactionBuckets = (
     buckets: Array<Array<Transaction>>,
     transaction: Transaction
-  ) => {
+  ): Array<Array<Transaction>> => {
     const lastBucket: Array<Transaction> = getOrAdd(buckets, last, []);
     const lastElement = last(lastBucket);
     if (lastElement === undefined || isDuplicated(lastElement, transaction)) {
       lastBucket.push(transaction);
     } else {
+      // return [...buckets, [transaction]]
       buckets.push([transaction]);
     }
+    return buckets;
   };
-  for (let transaction of sortedTransactions) {
-    putTransactionInLastCompatibleBucketOrNew(buckets, transaction);
-  }
-  const result = buckets.filter(it => it.length > 1);
 
-  return  result;
+  return transactions
+    .sort(compareTime)
+    .reduce(computeTransactionBuckets, [])
+    .filter(isLongerThan(1));
 };
